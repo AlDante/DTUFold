42b704b78618eca684951c2a21b6f8b7f1156d8c
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-05 01:38:10 +0000

    Adding numeric_owner as keyword arguement

--- colabfold.py
+++ colabfold.py
@@ -175,7 +175,7 @@ def run_mmseqs2(x, prefix, use_env=True, use_filter=True,
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
             
-                tar.extractall(path, members, numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
                 
             
             safe_extract(tar_gz, path)
